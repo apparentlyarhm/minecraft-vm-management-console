93638f831ebe67cb54e72924b538b6e76b113480
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-07-23 21:34:04 +0530

    config update for custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
+const isExport = process.env.OUTPUT_EXPORT === '1';
 
 const nextConfig: NextConfig = {
   basePath: isProd ? "/minecraft-vm-management-console" : "",
   assetPrefix: isProd ? "/minecraft-vm-management-console/" : "",
-  reactStrictMode: false
+  reactStrictMode: false,
+  ...(isExport ? { output: 'export' } : {})
+
 };
 
 
