734a1a2629a1c2a6de4d004416eabd067d06530f
Shah Mohd Arhum <mohd.arhum28@gmail.com>
2025-11-14 17:37:27 +0530

    change order of tabs

--- lib/config/tabsConfig.ts
+++ lib/config/tabsConfig.ts
@@ -1,8 +1,8 @@
 import { Monitor, Globe, FileCog, ShieldUser } from "lucide-react";
 
 export const tabs = [
-  { value: "details", label: "Details", icon: Monitor },
   { value: "MOTD", label: "MOTD", icon: Globe },
+  { value: "details", label: "Details", icon: Monitor },
   { value: "modlist", label: "Mod List", icon: FileCog },
   { value: "admin-controls", label: "Admin", icon: ShieldUser }
 ];
